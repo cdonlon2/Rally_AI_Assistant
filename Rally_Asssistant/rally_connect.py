528fe4789c02c461b55fa5b14b069d1c2fae0764
cdonlon2 <157008470+cdonlon2@users.noreply.github.com>
2024-10-04 15:04:11 +0100

    commit 1

--- Rally_Asssistant/rally_connect.py
+++ Rally_Asssistant/rally_connect.py
@@ -1,26 +1,30 @@
 import os  
 from dotenv import load_dotenv
-from pyral import Rally, rallyWorkset  
+from pyral import Rally
 
 # Replace with your Rally API key  
 API_KEY=os.getenv("RALLY_API_KEY"),
 
 # Rally server and workspace details  
 RALLY_SERVER = "rally1.rallydev.com"  
-WORKSPACE = "OPTUM"  # Replace with your workspace name if known  
+WORKSPACE = "Optum"  # Replace with your workspace name if known  
 
 # Initialize Rally object  
-rally = Rally(server=RALLY_SERVER, apikey=API_KEY, workspace=WORKSPACE)  
+rally = Rally(server="rally1.rallydev.com", apikey="_oQYm9R1VSkCWO3p6ecD2VeDAs6Jf3UG5ifrDABpwm8U", workspace='Optum', project='U2 Commercial Analytic  Engineering New Implementations')
 
-# Query for projects  
-response = rally.get('Project', fetch=True)  
+response = rally.get('UserStory', fetch=True, query='Iteration.Name = "Sprint 21"', projectScopeDown=True)
 
-# Check for errors  
-if response.errors:  
-    print("Errors encountered:")  
-    for error in response.errors:  
-        print(error)  
-else:  
-    # Print project details  
-    for project in response:  
-        print(f"Project Name: {project.Name}, Project ID: {project.ObjectID}")  
+# Iterate over the user stories and print their FormattedIDs and names
+for user_story in response:
+    print(f"{user_story.FormattedID}: {user_story.Name}: {user_story.Description}: {user_story.AcceptanceCriteria}")
+    print("*****")
+
+response = rally.get('UserStory', fetch=True, querry='FormattedID = US418943')
+story = response.next()
+#Iterate over the fields associated with the story
+for attr_name in dir(story):
+    if not attr_name.startswith('_'):
+        attr_value = getattr(story, attr_name)
+        if not callable(attr_value):
+            print(f"{attr_name}: {attr_value}")
+            print("*****")
